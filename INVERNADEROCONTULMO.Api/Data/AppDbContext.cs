19d45951b2d6eac1aa296d7ebabfae00cd93c5ee
Iván <shino09@users.noreply.github.com>
2026-07-31 17:01:07 -0400

    quitar  bd inmemory

--- INVERNADEROCONTULMO.Api/Data/AppDbContext.cs
+++ INVERNADEROCONTULMO.Api/Data/AppDbContext.cs
@@ -39,5 +39,42 @@ public class AppDbContext : DbContext
         modelBuilder.Entity<Pedido>().HasMany(p => p.Detalles).WithOne(d => d.Pedido).HasForeignKey(d => d.PedidoId);
         modelBuilder.Entity<Venta>().HasMany(v => v.Detalles).WithOne(d => d.Venta).HasForeignKey(d => d.VentaId);
         modelBuilder.Entity<Compra>().HasMany(c => c.Detalles).WithOne(d => d.Compra).HasForeignKey(d => d.CompraId);
+
+        // Mapea las propiedades C# a columnas Oracle en UPPER_SNAKE_CASE
+        // (ej: PasswordHash -> PASSWORD_HASH, FechaCreacion -> FECHA_CREACION)
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            foreach (var property in entityType.GetProperties())
+            {
+                property.SetColumnName(ToOracleColumn(property.Name));
+            }
+        }
+
+        // Oracle no soporta bool nativo: se mapea como NUMBER(1) (0 = false, 1 = true)
+        var entitiesWithActivo = new[]
+        {
+            modelBuilder.Entity<Usuario>().Metadata.Name,
+            modelBuilder.Entity<Producto>().Metadata.Name,
+            modelBuilder.Entity<Cliente>().Metadata.Name,
+            modelBuilder.Entity<Categoria>().Metadata.Name,
+            modelBuilder.Entity<Proveedor>().Metadata.Name
+        };
+        foreach (var entityName in entitiesWithActivo)
+        {
+            var entity = modelBuilder.Entity(entityName);
+            entity.Property("Activo").HasConversion<decimal>();
+        }
+    }
+
+    // Convierte un nombre PascalCase a UPPER_SNAKE_CASE
+    private static string ToOracleColumn(string name)
+    {
+        var builder = new System.Text.StringBuilder();
+        foreach (var c in name)
+        {
+            if (char.IsUpper(c) && builder.Length > 0) builder.Append('_');
+            builder.Append(char.ToUpperInvariant(c));
+        }
+        return builder.ToString();
     }
 }
